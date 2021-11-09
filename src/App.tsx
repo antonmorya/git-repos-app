fdc7671f8abb2c88876cea67360f7359ec1a6ce7
Anton Morya <anton.morya@gmail.com>
2021-11-09 17:49:19 +0200

    Initial commit (create-react-app, routes and Ant Design components)

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,129 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  Redirect,
+} from "react-router-dom";
+import { Layout, Menu } from "antd";
+import { SearchOutlined, StarOutlined } from "@ant-design/icons";
+import logo from "./logo.svg";
+
+const { Header, Content, Footer, Sider } = Layout;
+
+const ROUTES = {
+  repositories: "/repos",
+  oneRepo: "/repos/:id",
+  home: "/",
+  favourites: "/favourites",
+};
+
+const Repos = () => {
+  let { path, url, isExact, params } = useRouteMatch();
+  console.debug("Repos: ", { path, url, isExact, params });
+
+  return (
+    <Switch>
+      <Route exact path={path}>
+        <Redirect to={ROUTES.home} />
+      </Route>
+
+      <Route path={ROUTES.oneRepo}>
+        <h1>some repo</h1>
+      </Route>
+    </Switch>
+  );
+};
 
 function App() {
+  const [isCollapsed, setCollapsed] = useState<boolean>(true);
+
+  const isFavouritesPage = !!useRouteMatch({
+    path: ROUTES.favourites,
+    exact: true,
+  });
+  const isHomePage = !!useRouteMatch({ path: ROUTES.home, exact: true });
+
+  const defaultSelectedKey =
+    (isHomePage && "1") || (isFavouritesPage && "2") || "0";
+
+  console.debug({
+    isFavouritesPage,
+    isHomePage,
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Layout style={{ minHeight: "100vh" }}>
+      <Sider
+        collapsible
+        collapsed={isCollapsed}
+        onCollapse={() => setCollapsed((current: boolean) => !current)}
+      >
+        {/* <div className="logo" /> */}
+        <div
+          style={{
+            marginTop: 3,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <img
+            src={logo}
+            alt="Logo"
+            style={
+              {
+                // border: "1px dashed red",
+              }
+            }
+          />
+        </div>
+
+        <Menu
+          theme="dark"
+          defaultSelectedKeys={[defaultSelectedKey]}
+          mode="inline"
+        >
+          <Menu.Item key="1" icon={<SearchOutlined />}>
+            <Link to="/">Search</Link>
+          </Menu.Item>
+
+          <Menu.Item key="2" icon={<StarOutlined />}>
+            <Link to="/favourites">Favourites</Link>
+          </Menu.Item>
+        </Menu>
+      </Sider>
+
+      <Layout className="site-layout">
+        <Header className="site-layout-background" style={{ padding: 0 }} />
+
+        <Content style={{ margin: "0 16px" }}>
+          <div
+            className="site-layout-background"
+            style={{ padding: 24, minHeight: 360 }}
+          >
+            <Switch>
+              <Route exact path={ROUTES.home}>
+                <h1>Repos</h1>
+              </Route>
+
+              <Route path="/repos" component={Repos} />
+
+              <Route path={ROUTES.favourites}>
+                <h2>favourites</h2>
+              </Route>
+
+              <Route path="*">
+                <h1>No such route</h1>
+              </Route>
+            </Switch>
+          </div>
+        </Content>
+
+        <Footer style={{ textAlign: "center" }}>
+          Created by Morya Anton, using Ant Design components ©2021
+        </Footer>
+      </Layout>
+    </Layout>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  Redirect,
+} from "react-router-dom";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Router>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
